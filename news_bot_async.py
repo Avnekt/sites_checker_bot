c2a414dc8cdf1cd07f388dc8e1a35a9135f9ef2c
Avnekt <avnekt@avnekt.ru>
2022-10-10 16:21:44 +0500

    Изменен подход к загрузке конфигурации.

--- news_bot_async.py
+++ news_bot_async.py
@@ -9,7 +9,8 @@ import datetime
 import asyncio
 import aiohttp
 
-async def get_url_page(session, target_url):
+async def get_url_page(session, target_url, conf):
+    pattern = conf['pattern']
     ua = fua.UserAgent()
     ua.safari
     try:
@@ -22,33 +23,30 @@ async def get_url_page(session, target_url):
             except:
                 return 0
             for link in links:
-                if re.findall('.*ростелеком|\Wртк|^ртк|\s+цод|^цод|\Wсолар|осиевск.*', str(link).lower()):
+                if re.findall(f'.*{str(pattern)}.*', str(link).lower()):
                     if link['href'].startswith('https://'):
                         message_text = link['href']
                     else:
                         message_text = target_url + link['href']
-                    send_new_message(message_text)
+                    send_new_message(message_text, conf)
     except:
         print(f'[ERROR] Site processing is unsuccessful {target_url}')
         return 0
 #     print(f'[INFO] Site processing is successful {target_url}')
 
-async def gather_data():
+async def gather_data(conf):
     targets = []
-    with open('smi_list.txt', 'r') as smi_list:
-        line = smi_list.readline().replace('\n','')
-        while line:
-            if not line.startswith('#'):
-                targets.append(line)
-            line = smi_list.readline().replace('\n','')
+    for site in conf['sites_list']:
+        if site:
+            targets.append(site)
     async with aiohttp.ClientSession() as session:
         tasks = []
         for target in targets:
-            task = asyncio.create_task(get_url_page(session, target))
+            task = asyncio.create_task(get_url_page(session, target, conf))
             tasks.append(task)
         await asyncio.gather(*tasks)
 
-def send_new_message(message_text):
+def send_new_message(message_text, my_params):
     message_text = str(message_text)
     news = datetime.datetime.now().date().strftime('%d.%m.%Y') + ': ' + message_text
     with open('urls_news.txt', 'r') as was_sent:
@@ -59,17 +57,18 @@ def send_new_message(message_text):
             line = was_sent.readline()
         with open('urls_news.txt', 'a') as was_sent:
             was_sent.write(str(news) + '\n')
-    with open('tlgr.yml') as conf:
-        my_params = yaml.safe_load(conf)
     tgApiSend = 'https://api.telegram.org/bot' + my_params['tkn'] + '/sendMessage'
     payload = dict(chat_id=my_params['chat_id'], text=message_text)
     requests.post(tgApiSend, data=payload)
 
-def main():
+def main(params):
     time_sleep = random.randint(0, 18)
-    asyncio.run(gather_data())
+    asyncio.run(gather_data(params))
 
 if __name__ == '__main__':
     start_time = time.time()
-    main()
+    with open('config.yml') as conf:
+        my_params = yaml.safe_load(conf)
+#     print(my_params)
+    main(my_params)
     print(f'Затраченное время {round(time.time() - start_time, 1)} сек.')
